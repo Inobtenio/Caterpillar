c8e1c4d3ed0a53f1a7130549123c25a894d1f99c
Kevin Martin <kevin.martin@tektonlabs.com>
2017-02-26 00:24:14 -0500

    m

--- index.js
+++ index.js
@@ -23,6 +23,78 @@ app.get("*", function(req, res){
   res.sendFile(__dirname + '/views/index.html');
 });
 
+const electron = require('electron')
+const electron_app = electron.app
+const BrowserWindow = electron.BrowserWindow
+const url = require('url')
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let win
+
+function createWindow () {
+  // Create the browser window.
+  win = new BrowserWindow({
+    width: 800,
+    height: 120,
+    resizable: false,
+    webPreferences: {
+        nodeIntegration: false,
+        webSecurity: false,
+        plugins: true
+    }
+  })
+
+  // and load the index.html of the app.
+  // win.loadURL(url.format({
+  //   pathname: path.join(__dirname, '/views/index.html'),
+  //   protocol: 'file:',
+  //   slashes: true
+  // }))
+
+  win.loadURL("http://localhost:5000")
+
+  // Open the DevTools.
+  win.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+  win.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    win = null
+  })
+
+  win.webContents.on("devtools-opened", () => {
+    win.webContents.closeDevTools();
+  });
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+electron_app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+electron_app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    electron_app.quit()
+  }
+})
+
+electron_app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null) {
+    createWindow()
+  }
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
+
 function parseTime(number) {
   let fullseconds = Math.round(number);
   let minutes = Math.floor(fullseconds / 60);
